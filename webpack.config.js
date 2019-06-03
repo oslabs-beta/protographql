172be3542016b70651e3f4c497fff997e66bad05
Jarred <jackhajb@gmail.com>
2019-06-03 14:38:41 -0400

    Setup webpack-dev-server and "dev" script

--- webpack.config.js
+++ webpack.config.js
@@ -3,8 +3,9 @@ const path = require('path');
 module.exports = {
   entry: "./src/index.tsx",
   output: {
-      filename: "bundle.js",
-      path: __dirname + "/dist"
+    publicPath: '/dist/',
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js"
   },
 
   // Enable sourcemaps for debugging webpack's output.
@@ -28,14 +29,5 @@ module.exports = {
           // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
           { enforce: "pre", test: /\.js$/, loader: "source-map-loader" }
       ]
-  },
-
-  // When importing a module whose path matches one of the following, just
-  // assume a corresponding global variable exists and use that instead.
-  // This is important because it allows us to avoid bundling all of our
-  // dependencies, which allows browsers to cache those libraries between builds.
-  externals: {
-      "react": "React",
-      "react-dom": "ReactDOM"
   }
 };

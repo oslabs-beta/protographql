19a89fc82cd95df42fabe7213ff7fd3a5d97e22c
Jarred <jackhajb@gmail.com>
2019-06-03 14:03:32 -0400

    Renamed hello to App, changed root html el

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,9 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 
-import { Hello } from "./components/Hello";
+import { App } from "./components/App";
 
 ReactDOM.render(
-    <Hello compiler="TypeScript" framework="React" />,
-    document.getElementById("example")
+    <App compiler="TypeScript" framework="React" />,
+    document.getElementById("root")
 );

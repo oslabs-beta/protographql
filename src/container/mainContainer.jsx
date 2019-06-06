192d9734b75458be3eb5330a07f9e9c866b32449
Rodolfoguzman25 <rodolfoguzman147@gmail.com>
2019-06-05 20:19:27 -0400

    small adjustments after merge

--- src/container/mainContainer.jsx
+++ src/container/mainContainer.jsx
@@ -48,11 +48,9 @@ const Main = () => {
   //Rendered components and elements
   return (
     <div>
-      <h1> Our cool ProtoGraphQL App </h1>
-
       {/* uncomment the components to test */}
-      {/* <Header /> */}
       <NavSideBar />
+      <Header />
       <Welcome />
     </div>
   )

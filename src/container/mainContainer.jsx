2ac63456336e8d4a975da86c357f9763da71acd9
Bryan Fong <bryanfong.dev@gmail.com>
2019-06-09 16:49:33 -0400

    refactored Main Container - thanks jarred for the idea

--- src/container/mainContainer.jsx
+++ src/container/mainContainer.jsx
@@ -3,51 +3,24 @@ import Header from '../components/header/header';
 import NavSideBar from '../components/navSideBar/navSidebar';
 import Welcome from '../components/popup/welcome';
 import TableDetailView from '../components/view/tableDetailView';
-import * as state from '../state/initialState';
-import * as mockState from '../state/mockState';
-import { withStyles, makeStyles } from '@material-ui/core/styles';
-import { Grid } from '@material-ui/core';
+// import * as state from '../state/initialState';
+import * as state from '../state/mockState';
 import MainView from '../components/view/mainView';
 import { relative } from 'path';
 
-
+/* Comments
+  SetView toggles the tab shown in the sandbox area
+  potential tabs for MVP are code & schema, stretch would include GQL setup area
+  setPopUp toggle popups
+  potential popups are welcome, table details, and export (select folder to save & success)
+*/
 const Main = () => {
-
-  //State constants instantiated using useState
-  //Clean initial state
-  /*
   const [selectedTable, setSelectedTable] = useState(state.selectedTableState);
-
   const [selectedField, setSelectedField] = useState(state.selectedFieldState);
-
   const [tableIndex, setTableIndex] = useState(state.tableIndexState);
-
-  const [tables,setTables] = useState(state.tablesState);
-
-
+  const [tables, setTables] = useState(state.tablesState);
   const [view, setView] = useState(state.viewState);
-
-
   const [popUp, setPopUp] = useState(state.popUpState);
-
-  */
-
-  //currently pointing to mock state data
-  const [selectedTable, setSelectedTable] = useState(mockState.selectedTableState);
-
-  const [selectedField, setSelectedField] = useState(mockState.selectedFieldState);
-
-  const [tableIndex, setTableIndex] = useState(mockState.tableIndexState);
-
-  const [tables, setTables] = useState(mockState.tablesState);
-
-  //this will toggle the tab shown in the sandbox area
-  //potential tabs for MVP are code & schema, stretch would include GQL setup area
-  const [view, setView] = useState(mockState.viewState);
-
-  //this will toggle popups
-  //potential popups are welcome, table details, and export (select folder to save & success)
-  const [popUp, setPopUp] = useState(mockState.popUpState);
 
   //Rendered components and elements
   // can't use styled components here because of the gridTemplateAreas
@@ -57,16 +30,15 @@ const Main = () => {
       gridTemplateColumns: "1fr 1fr 1fr 1fr 1fr 1fr 1fr",
       gridTemplateRows: "65px auto",
       gridTemplateAreas: `
-          "header header header header header header header"
-          "navSideBar main main main main main main"
-        `,
+        "header header header header header header header"
+        "navSideBar main main main main main main"`,
       height: "100vh",
       backgroundColor: "#EEEFF0",
       fontFamily: "'Roboto', sans-serif",
     }}>
       <Header />
       {/* <Welcome popUp={popUp} setPopUp={setPopUp} /> */}
-      <NavSideBar setView={setView} setPopUp={setPopUp}/>
+      <NavSideBar setView={setView} setPopUp={setPopUp} />
       <MainView
         view={view}
         tables={tables}

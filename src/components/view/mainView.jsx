af139d7e469d49dec2bac307e336427783beb4a6
Bryan Fong <bryanfong.dev@gmail.com>
2019-06-09 18:35:07 -0400

    deleted console.log in mainView

--- src/components/view/mainView.jsx
+++ src/components/view/mainView.jsx
@@ -5,10 +5,6 @@ import { Container } from '@material-ui/core';
 import TableForm from '../view/tableForm';
 
 function MainView({ view, tables, setTables, setPopUp, setView, popUp }) {
-  // console.log('setView: ', setView);
-  // console.log('setPopUp: ', setPopUp);
-  // console.log('setTables: ', setTables);
-  // console.log('tables: ', tables);
   return (
     <div style={{ gridArea: "main" }}>
       {view === 'code' && <CodeView tables={tables} />}
@@ -19,7 +15,7 @@ function MainView({ view, tables, setTables, setPopUp, setView, popUp }) {
         setView={setView}
         view={view}
       />}
-      {view === 'table' && <TableForm />}
+      {/* {view === 'table' && <TableForm />} */}
       {popUp === 'table' && <TableForm setPopUp={setPopUp} />}
     </div>
   )

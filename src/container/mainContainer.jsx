0be27728404d9bef575a8dfdcf21d6c6d8447a12
Bryan Fong <bryanfong.dev@gmail.com>
2019-06-09 16:35:13 -0400

    refactored main container

--- src/container/mainContainer.jsx
+++ src/container/mainContainer.jsx
@@ -10,63 +10,38 @@ import { Grid } from '@material-ui/core';
 import MainView from '../components/view/mainView';
 import { relative } from 'path';
 
+//change to state for production
+const stateMode = "MockState";
 
 const Main = () => {
-
-  //State constants instantiated using useState
-  //Clean initial state
-  /*
-  const [selectedTable, setSelectedTable] = useState(state.selectedTableState);
-
-  const [selectedField, setSelectedField] = useState(state.selectedFieldState);
-
-  const [tableIndex, setTableIndex] = useState(state.tableIndexState);
-
-  const [tables,setTables] = useState(state.tablesState);
-
-
-  const [view, setView] = useState(state.viewState);
-
-
-  const [popUp, setPopUp] = useState(state.popUpState);
-
-  */
-
-  //currently pointing to mock state data
-  const [selectedTable, setSelectedTable] = useState(mockState.selectedTableState);
-
-  const [selectedField, setSelectedField] = useState(mockState.selectedFieldState);
-
-  const [tableIndex, setTableIndex] = useState(mockState.tableIndexState);
-
-  const [tables, setTables] = useState(mockState.tablesState);
-
-  //this will toggle the tab shown in the sandbox area
-  //potential tabs for MVP are code & schema, stretch would include GQL setup area
-  const [view, setView] = useState(mockState.viewState);
-
-  //this will toggle popups
+  //setView will change the tab shown in the sandbox
+  //potential tabs for MVP are code & schema, stretch would include GQL setup area)
+  //setPopup will toggle popups 
   //potential popups are welcome, table details, and export (select folder to save & success)
-  const [popUp, setPopUp] = useState(mockState.popUpState);
+  const [selectedTable, setSelectedTable] = useState({ stateMode }.selectedTableState);
+  const [selectedField, setSelectedField] = useState({ stateMode }.selectedFieldState);
+  const [tableIndex, setTableIndex] = useState({ stateMode }.tableIndexState);
+  const [tables, setTables] = useState({ stateMode }.tablesState);
+  const [view, setView] = useState({ stateMode }.viewState);
+  const [popUp, setPopUp] = useState({ stateMode }.popUpState);
 
   //Rendered components and elements
-  // can't use styled components here because of the gridTemplateAreas
   return (
     <div style={{
       display: "grid",
       gridTemplateColumns: "1fr 1fr 1fr 1fr 1fr 1fr 1fr",
-      gridTemplateRows: "65px auto",
+      gridTemplateRows: "70px auto",
       gridTemplateAreas: `
           "header header header header header header header"
           "navSideBar main main main main main main"
         `,
       height: "100vh",
-      backgroundColor: "#EEEFF0",
-      fontFamily: "'Roboto', sans-serif",
+      backgroundColor: "white",
+      fontFamily: "'Roboto', sans-serif"
     }}>
       <Header />
-      {/* <Welcome popUp={popUp} setPopUp={setPopUp} /> */}
-      <NavSideBar setView={setView} setPopUp={setPopUp}/>
+      <Welcome popUp={popUp} setPopUp={setPopUp} />
+      <NavSideBar setView={setView} setPopUp={setPopUp} />
       <MainView
         view={view}
         tables={tables}

6e2fad2b6bcf4aa9ff5448c46cdbe477c3bfa296
Bryan Fong <bryanfong.dev@gmail.com>
2019-06-24 20:29:22 -0400

    Added a snackbar for demo purposes - will need to customize
    Co-authored-by: Geoffrey Lin <geoffrey.s.lin@gmail.com>

--- src/components/view/schemaView.jsx
+++ src/components/view/schemaView.jsx
@@ -2,10 +2,10 @@ import React, { useContext } from 'react';
 import SchemaTable from './schemaTable';
 import styled from 'styled-components';
 import { Store } from '../../state/store';
-import { 
-  SET_POP_UP, 
-  SET_VIEW, 
-  EDIT_TABLE, 
+import {
+  SET_POP_UP,
+  SET_VIEW,
+  EDIT_TABLE,
   DELETE_TABLE
 } from '../../actions/actionTypes';
 
@@ -29,17 +29,25 @@ function SchemaView() {
       key={tableKey}
       tableKey={tableKey}
       table={tables[tableKey]}
-      setPopUp={ payload => dispatch({ type: SET_POP_UP, payload }) }
-      setView={ payload => dispatch({ type: SET_VIEW, payload }) }
+      setPopUp={payload => dispatch({ type: SET_POP_UP, payload })}
+      setView={payload => dispatch({ type: SET_VIEW, payload })}
       style={{ margin: "10px" }}
-      deleteTable={ payload => dispatch({ type: DELETE_TABLE, payload })}
-      editTable={ payload => dispatch({ type: EDIT_TABLE, payload }) }
+      deleteTable={payload => dispatch({ type: DELETE_TABLE, payload })}
+      editTable={payload => dispatch({ type: EDIT_TABLE, payload })}
     />
   ))
 
   return (
     <View >
       {tablesArray}
+      <button onClick={
+        () => {
+          var x = document.getElementById("snackbar");
+          x.className = "show";
+          setTimeout(function () { x.className = x.className.replace("show", ""); }, 3000);
+        }
+      }>Show Snackbar</button>
+      <div id="snackbar">Cannot delete</div>
     </View>
   )
 }

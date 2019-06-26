83fb4265836c8a2abd4764ac474fdea8e3be1988
Alena Budzko <budzko.alena@gmail.com>
2019-06-26 19:39:01 -0400

    Completed collapsable D# tree visualizer

--- src/components/view/visualizeView.jsx
+++ src/components/view/visualizeView.jsx
@@ -30,140 +30,6 @@ const Viz = styled.div`
 
 /*-------------------- Functional Component --------------------*/
 
-// function VisualizeView() {
-
-//   const { state: { visualizeJSON } } = useContext(Store);
-
-//   function responsivefy(svg) {
-//     var container = d3.select(svg.node().parentNode),
-//       width = parseInt(svg.style("width")),
-//       height = parseInt(svg.style("height")),
-//       aspect = width / height;
-//     svg.attr("viewBox", "0 0 " + width + " " + height)
-//       .attr("perserveAspectRatio", "xMinYMid")
-//       .call(resize);
-//     d3.select(window).on("resize." + container.attr("id"), resize);
-
-//     function resize() {
-//       var targetWidth = parseInt(container.style("width"));
-//       svg.attr("width", targetWidth);
-//       svg.attr("height", Math.round(targetWidth / aspect));
-//     }
-//   }
-
-//   const createViz = () => {
-//     const svg = d3.select('#vizView').append('svg');
-//     const width = 700;
-//     const height = 600;
-
-//     const margin = {top: 0, right: 130, bottom: 0, left: 60};
-//     const innerWidth = width - margin.left - margin.right;
-//     const innerHeight = height - margin.top - margin.bottom;
-    
-//     const tree = d3.tree().size([innerHeight, innerWidth]);
-
-//     const g = svg
-//       .attr('width', width)
-//       .attr('height', height)
-//       .call(responsivefy)
-//       .append('g')
-//         .attr('transform', `translate(${margin.left})`)
-      
-//     const root = d3.hierarchy(visualizeJSON);
-
-//     const links = tree(root).links();
-//     const LinkGenerator = d3.linkHorizontal()
-//       .x(d => d.y)
-//       .y(d => d.x)
-    
-//     g.selectAll('path').data(links)
-//       .enter()
-//       .append('path')
-//       .attr('d', LinkGenerator)
-//       .attr('fill', 'none')
-//       .attr('stroke', '#DD399C')
-//       .attr('stroke-width', 2)
-//       .attr('stroke-opacity', '0.65')
-        
-//     const shapes = g.selectAll('shapes')
-//       .data(root.descendants()).enter()
-
-//     shapes.append('ellipse')
-//       .filter(d => d.depth % 2 === 0)
-//       .attr('class', 'ellipse')
-//       .attr('cx', d => {
-//         if (d.depth === 3) return d.y + 55;
-//         return d.y;
-//       })
-//       .attr('cy', d => d.x)
-//       .attr('rx', 50)
-//       .attr('ry', 20)
-//       .attr('stroke', 'rgba(0, 0, 0, 0.12')
-//       .attr('stroke-width', 0.5)
-//       .attr('fill', 'white')
-
-//     shapes.append('rect')
-//       .filter(d => d.depth % 2 === 1)
-//       .attr('class', 'rect')
-//       .attr('x', d => {
-//         if (d.depth === 3) return d.y - 10;
-//         return d.y - 60;
-//       })
-//       .attr('y', d => d.x - 15)
-//       .attr('width', 120)
-//       .attr('height', 30)
-//       .attr('stroke', 'rgba(0, 0, 0, 0.12')
-//       .attr('stroke-width', 0.5)
-//       .attr('fill', 'white')
-
-//     g.selectAll('text').data(root.descendants())
-//     .enter().append('text')
-//       .attr('x', d => d.y)
-//       .attr('y', d => d.x)
-//       .attr('dy', '0.32em')
-//       .attr('text-anchor', d => d.depth === 3 ? 'start' : 'middle')
-//       .text(d => {
-//         if (d.depth === 0 || d.depth === 2) return d.data.name;
-//         return `${d.data.name}: ${d.data.type}`;
-//       })
-//       .attr('font-family', 'Helvetica')
-//       .attr('font-size', '0.8em')
-//       .attr('cursor', 'pointer')
-
-//     // GLOW effect
-//     const defs = svg.append("defs");
-
-//     const filter = defs.append("filter")
-//       .attr("id","glow");
-//     filter.append("feGaussianBlur")
-//       .attr("stdDeviation","5")
-//       .attr("result","coloredBlur");
-    
-//     const feMerge = filter.append("feMerge");
-//     feMerge.append("feMergeNode")
-//       .attr("in","coloredBlur");
-//     feMerge.append("feMergeNode")
-//       .attr("in","SourceGraphic");
-
-//     d3.selectAll(".ellipse")
-//       .style("filter", "url(#glow)");
-
-//     d3.selectAll(".rect")
-//       .style("filter", "url(#glow)");
-
-//   }
-  
-//   useEffect(createViz,[])
-
-//   return (
-//     <Container>
-//       <Viz id="vizView"></Viz>
-//       <VisualizerSideBar></VisualizerSideBar>
-//     </Container>   
-    
-//   );
-// }
-
 function VisualizeView() {
 
   const { state: { visualizeJSON } } = useContext(Store);
@@ -172,21 +38,41 @@ function VisualizeView() {
 
   function createViz() {
 
-    var margin = {top: 20, right: 90, bottom: 30, left: 90},
-    width = 960 - margin.left - margin.right,
-    height = 500 - margin.top - margin.bottom;
+    function responsivefy(svg) {
+      const container = d3.select(svg.node().parentNode),
+        width = parseInt(svg.style("width")),
+        height = parseInt(svg.style("height")),
+        aspect = width / height;
+      svg.attr("viewBox", "0 0 " + width + " " + height)
+        .attr("perserveAspectRatio", "xMinYMid")
+        .call(resize);
+      d3.select(window).on("resize." + container.attr("id"), resize);
+  
+      function resize() {
+        const targetWidth = parseInt(container.style("width"));
+        svg.attr("width", targetWidth);
+        svg.attr("height", Math.round(targetWidth / aspect));
+      }
+    }
 
-    var svg = d3.select("#vizView").append("svg")
+    const fillColor = "#F5FDFD"
+
+    const margin = {top: 20, right: 120, bottom: 20, left: 80},
+          width = 700 - margin.left - margin.right,
+          height = 600 - margin.top - margin.bottom;
+
+    const svg = d3.select("#vizView").append("svg")
     .attr("width", width + margin.right + margin.left)
     .attr("height", height + margin.top + margin.bottom)
-      .append("g")
-        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+    .call(responsivefy)
+    .append("g")
+      .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
 
-    var i = 0,
-        duration = 750,
-        root;
+    let i = 0;
+    const duration = 750;
+    let root;
 
-    var treemap = d3.tree().size([height, width]);
+    const treemap = d3.tree().size([height, width]);
 
     root = d3.hierarchy(treeData, function(d) { return d.children; });
     root.x0 = height / 2;
@@ -205,16 +91,16 @@ function VisualizeView() {
 
     function update(source) {
 
-      var treeData = treemap(root);
-      var nodes = treeData.descendants();
-      var links = treeData.descendants().slice(1);
+      const treeData = treemap(root);
+      const nodes = treeData.descendants();
+      const links = treeData.descendants().slice(1);
       
       nodes.forEach(function(d){ d.y = d.depth * 180});
 
-      var node = svg.selectAll('g.node')
+      const node = svg.selectAll('g.node')
         .data(nodes, function(d) {return d.id || (d.id = ++i); });
 
-      var nodeEnter = node.enter().append('g')
+      const nodeEnter = node.enter().append('g')
         .attr('class', 'node')
         .attr("transform", function(d) {
           return "translate(" + source.y0 + "," + source.x0 + ")";
@@ -224,7 +110,7 @@ function VisualizeView() {
       nodeEnter.append('ellipse')
         .attr('class', 'ellipse')
         .style("fill", function(d) {
-          return d._children ? "lightsteelblue" : "#fff";
+          return d._children ? fillColor : "#fff";
         })
 
       nodeEnter.append('text')
@@ -237,7 +123,7 @@ function VisualizeView() {
         .attr('font-size', '0.7em')
         .attr('cursor', 'pointer');
 
-      var nodeUpdate = nodeEnter.merge(node);
+      const nodeUpdate = nodeEnter.merge(node);
       
       nodeUpdate.transition()
         .duration(duration)
@@ -246,13 +132,12 @@ function VisualizeView() {
         });
 
       nodeUpdate.select('ellipse.node')
-        .attr('r', 10)
         .style("fill", function(d) {
-          return d._children ? "lightsteelblue" : "#fff";
+          return d._children ? fillColor : "#fff";
         })
         .attr('cursor', 'pointer');
 
-      var nodeExit = node.exit().transition()
+      const nodeExit = node.exit().transition()
         .duration(duration)
         .attr("transform", function(d) {
           return "translate(" + source.y + "," + source.x + ")";
@@ -266,26 +151,26 @@ function VisualizeView() {
       nodeExit.select('text')
         .style('fill-opacity', 1e-6);
 
-      var link = svg.selectAll('path.link')
+      const link = svg.selectAll('path.link')
         .data(links, function(d) { return d.id; });
 
-      var linkEnter = link.enter().insert('path', "g")
+      const linkEnter = link.enter().insert('path', "g")
         .attr("class", "link")
         .attr('d', function(d){
-          var o = {x: source.x0, y: source.y0}
+          const o = {x: source.x0, y: source.y0}
           return diagonal(o, o)
         });
 
-      var linkUpdate = linkEnter.merge(link);
+      const linkUpdate = linkEnter.merge(link);
 
       linkUpdate.transition()
         .duration(duration)
         .attr('d', function(d){ return diagonal(d, d.parent) });
 
-      var linkExit = link.exit().transition()
+      const linkExit = link.exit().transition()
         .duration(duration)
         .attr('d', function(d) {
-          var o = {x: source.x, y: source.y}
+          const o = {x: source.x, y: source.y}
           return diagonal(o, o)
         })
         .remove();
@@ -320,7 +205,7 @@ function VisualizeView() {
         .attr('fill', 'none')
         .attr('stroke', '#DD399C')
         .attr('stroke-width', 2)
-        .attr('stroke-opacity', '0.65')
+        .attr('stroke-opacity', '1')
 
       d3.selectAll('ellipse')
         .attr('rx', 60)
@@ -331,21 +216,124 @@ function VisualizeView() {
 
       // GLOW effect
       const defs = svg.append("defs");
+      const colors = ["#75E1CD", "#FDFF9E", "#C5AAFF"]
 
-      const filter = defs.append("filter")
-        .attr("id","glow");
-      filter.append("feGaussianBlur")
-        .attr("stdDeviation","5")
+      //set color (white glow) for parent node
+      const filter0 = defs.append("filter")
+        .attr("id","glow0");
+      filter0.append("feGaussianBlur")
+        .attr("stdDeviation", "5")
         .attr("result","coloredBlur");
+      const feMerge0 = filter0.append("feMerge");
+      feMerge0.append("feMergeNode")
+        .attr("in", "coloredBlur");
+      feMerge0.append("feMergeNode")
+        .attr("in", "SourceGraphic");
+
+      // set color for queries
+      const filter1 = defs.append("filter")
+        .attr("id","glow1");
+
+      filter1.append("feMorphology")
+        .attr("operator", "dilate")
+        .attr("radius", 1.5)
+        .attr("in", "SourceAlpha")
+        .attr("result", "thicken");
+
+      filter1.append("feGaussianBlur")
+        .attr("in", "thicken")
+        .attr("stdDeviation","2")
+        .attr("result","blurred");
+
+      filter1.append("feFlood")
+        .attr("flood-color", colors[0])
+        .attr("result", "glowColor")
+
+      filter1.append("feComposite")
+        .attr("in", "glowColor")
+        .attr("in2", "blurred")
+        .attr("operator", "in")
+        .attr("result", "softGlow_colored")
       
-      const feMerge = filter.append("feMerge");
-      feMerge.append("feMergeNode")
-        .attr("in","coloredBlur");
-      feMerge.append("feMergeNode")
+      const feMerge1 = filter1.append("feMerge");
+      feMerge1.append("feMergeNode")
+        .attr("in","softGlow_colored");
+      feMerge1.append("feMergeNode")
         .attr("in","SourceGraphic");
 
+      // set color for types
+      const filter2 = defs.append("filter")
+      .attr("id","glow2");
+
+      filter2.append("feMorphology")
+        .attr("operator", "dilate")
+        .attr("radius", 1.5)
+        .attr("in", "SourceAlpha")
+        .attr("result", "thicken");
+
+      filter2.append("feGaussianBlur")
+        .attr("in", "thicken")
+        .attr("stdDeviation","2")
+        .attr("result","blurred");
+
+      filter2.append("feFlood")
+        .attr("flood-color", colors[1])
+        .attr("result", "glowColor")
+
+      filter2.append("feComposite")
+        .attr("in", "glowColor")
+        .attr("in2", "blurred")
+        .attr("operator", "in")
+        .attr("result", "softGlow_colored")
+      
+      const feMerge2 = filter2.append("feMerge");
+      feMerge2.append("feMergeNode")
+        .attr("in","softGlow_colored");
+      feMerge2.append("feMergeNode")
+        .attr("in","SourceGraphic");
+
+      // set color for queryable
+      const filter3 = defs.append("filter")
+      .attr("id","glow3");
+
+      filter3.append("feMorphology")
+        .attr("operator", "dilate")
+        .attr("radius", 1.5)
+        .attr("in", "SourceAlpha")
+        .attr("result", "thicken");
+
+      filter3.append("feGaussianBlur")
+        .attr("in", "thicken")
+        .attr("stdDeviation","2")
+        .attr("result","blurred");
+
+      filter3.append("feFlood")
+        .attr("flood-color", colors[2])
+        .attr("result", "glowColor")
+
+      filter3.append("feComposite")
+        .attr("in", "glowColor")
+        .attr("in2", "blurred")
+        .attr("operator", "in")
+        .attr("result", "softGlow_colored")
+      
+      const feMerge3 = filter3.append("feMerge");
+      feMerge3.append("feMergeNode")
+        .attr("in","softGlow_colored");
+      feMerge3.append("feMergeNode")
+        .attr("in","SourceGraphic");
+      
+      
       d3.selectAll(".ellipse")
-        .style("filter", "url(#glow)");
+        .style("filter", (d) => {
+          if (d.depth === 0) return "url(#glow0)";
+          if (d.depth === 1) return "url(#glow1)";
+          if (d.depth === 2) return "url(#glow2)";
+          if (d.depth === 3) return "url(#glow3)";
+        })
+        .style("fill", function(d) {
+          return d._children ? fillColor : "#fff";
+        });
 
     }
   }

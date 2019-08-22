950dfd241c4a4ff43f418c9d274242d4c709887e
Haris Hambasic <harishambasic@Hariss-MacBook.local>
2019-08-22 15:03:00 -0700

    Commenting Update V1.1

--- src/components/view/mainView.jsx
+++ src/components/view/mainView.jsx
@@ -6,7 +6,12 @@ import TableForm from '../popup/tableForm';
 import { Store } from '../../state/store';
 
 function MainView() {
-
+  /*
+    -> connects the application to the context (utilized by Hooks in React) and facilitates the ability to
+      update the context of the application
+    -> the context is initialized by useContext() and specified by Store which is found
+      in /components/state/store.jsx
+  */
   const { state: { view, popUp } } = useContext(Store);
   return (
     <div style={{ gridArea: "main", overflowX: "auto", }}>

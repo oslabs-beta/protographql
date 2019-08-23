ec2385bc2de4864ad7a52407322e642503167a5e
Milmoody <moody.lillian@gmail.com>
2019-08-23 16:16:50 -0700

    test apollo-server changes merged with current master - breaking changes

--- src/index.jsx
+++ src/index.jsx
@@ -3,7 +3,9 @@ import * as ReactDOM from "react-dom";
 
 import { App } from "./App";
 
-ReactDOM.render(
+// ReactDOM.render(     //uncomment for regular serverless rendering
+ReactDOM.hydrate(        //uncomment for express rendering
     <App />,
     document.getElementById("root")
 );
+

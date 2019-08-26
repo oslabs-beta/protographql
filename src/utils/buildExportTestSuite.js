e665d38f7707e24b13e2e14d87f453a6ab032078
Haris Hambasic <harishambasic@Hariss-MacBook.local>
2019-08-26 15:31:09 -0700

    Application has been updated to generate tests based on user input.

--- src/utils/buildExportTestSuite.js
+++ src/utils/buildExportTestSuite.js
@@ -1,47 +1,14 @@
 let fs = require('fs');
 let archiver = require('archiver');
 
-// hard-coded query data (will be replaced with dynamic data as soon as the code is ready)
-let query = ['SELECT * FROM Employees', 'SELECT * FROM Employees WHERE name=\"Haris\"'];
-
-// hard-coded response data (will be replaced with dynamic data as soon as the code is ready)
-let response = [
-    ['{name:\'Haris\', age:23}, {name:\'Shaun\', age:26}'],
-    ['{name:\'Haris\', age:23}']
-];
-
-// iterate over the number of queries and write to the test file each test
-for (let i=0; i<query.length; i++) {
-    fs.appendFileSync('test-suite.js', 'test(\'query\', () => {\n\texpect(\''+query[i]+'\').toEqual('+response[i]+');\n});\n\n');
-}
-
-let output = fs.createWriteStream('/Users/harishambasic/Downloads' + '/protographqlTestSuite.zip');
-
-var archive = archiver('zip', {
-    zlib: { level: 9 } // set the compression level
-});
-
-// when the archiver functionality is complete and no errors have occurred, outout 'zip complete'
-output.on('close', function () {
-    console.log('zip complete');
-});
-
-archive.on('error', function(err){
-    console.log(err);
-});
-
-// pipe archive data to the file
-archive.pipe(output);
-
-archive.append(fs.createReadStream(__dirname + '/test-suite.js'), { name: '/tests-suite/test-suite.js' });
-
-// delete the 'test-suite.js' file
-fs.unlink('test-suite.js', function(err) {
-    if (err) {
-        throw err;
+exports.createTest = function(queries, responses) {
+    let tests = '';
+    for (let i=0; i<queries.length; i++) {
+        if (i > 0) {
+            tests = tests.concat('\n\ntest(\'query\', () => {\n\texpect(\''+queries[i]+'\').toEqual(\''+responses[i]+'\')\n})');
+        } else {
+            tests = tests.concat('test(\'query\', () => {\n\texpect(\''+queries[i]+'\').toEqual(\''+responses[i]+'\')\n})');
+        }
     }
-});
-
-// finalize the archive (i.e: we are done appending files but streams have to finish yet)
-// 'close', 'end' or 'finish' may be fired right after calling this method so register to them beforehand
-archive.finalize();
+    return tests;
+};

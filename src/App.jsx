99e2aa1e663a4ce1ddbce21ceab5614ceeefc182
Milmoody <moody.lillian@gmail.com>
2019-08-26 22:35:25 -0700

    added apollo client running in background, queries submitted will hit endpoint

--- src/App.jsx
+++ src/App.jsx
@@ -2,35 +2,6 @@ import * as React from "react";
 import Main from './container/mainContainer';
 import { StoreProvider } from './state/store';
 
-import { ApolloClient } from 'apollo-client';
-import { InMemoryCache } from 'apollo-cache-inmemory';
-import { gql, HttpLink } from 'apollo-boost';
-
-// const link = createIpcLink({ ipc: ipcRenderer });
-
-const link = new HttpLink({
-  uri: 'http://localhost:3000/GraphQL'
-})
-
-const client = new ApolloClient({
-  cache: new InMemoryCache(),
-  link: link
-});
-
-console.log("client: ",client)
-
-// testing Apollo Server with query - currently not working
-client.query({
-    query: gql`
-        {
-            getAllAuthor {
-                last_name
-            }
-        }`
-}).then(result => console.log('apollo server result: ',result))
-
-console.log("client after: ", client)
-
 export const App = () => (
     <StoreProvider>
         <Main />
--- src/components/view/testsView.jsx
+++ src/components/view/testsView.jsx
@@ -5,8 +5,38 @@ import {
     UPDATE_QUERIES
 } from '../../actions/actionTypes';
 
-const electron = window.require('electron');
-const ipc = electron.ipcRenderer;
+// const electron = window.require('electron');
+// const ipc = electron.ipcRenderer;
+
+
+import { ApolloClient } from 'apollo-client';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { gql, HttpLink } from 'apollo-boost';
+
+// const link = createIpcLink({ ipc: ipcRenderer });
+
+const link = new HttpLink({
+  uri: 'http://localhost:3000/GraphQL'
+})
+
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  link: link
+});
+
+// console.log("client: ",client)
+
+// // testing Apollo Server with query - currently not working
+// client.query({
+//     query: gql`
+        // {
+        //     getAllAuthor {
+        //         last_name
+        //     }
+        // }`
+// }).then(result => console.log('apollo server result: ',result))
+
+// console.log("client after: ", client)
 
 const Title = styled.h1`
 font-size: 1.5em;
@@ -14,13 +44,30 @@ text-align: center;
 color: palevioletred;
 `;
 
+const Response = styled.div`
+    margin: 13px;
+    font-family: Courier New, Consolas, Monaco, Lucida Console;
+    font-size: 15px;
+    background-color: #EFF0F1;
+    display: grid;
+    height: calc(100vh - 26px - 64px);
+`;
+
 function TestsView() {
     const { dispatch, state: { queries }} = useContext(Store);
 
     function addQuery() {
         let q = document.getElementById("query").value;
-        let r = document.getElementById("response").value;
-        dispatch({ type: UPDATE_QUERIES, payload: [[q], [r]] });
+        let r;
+        client.query({
+            query: gql`${q}`
+        }).then(result => {;
+            console.log('apollo server result: ',result);
+            r = result.data;
+            dispatch({ type: UPDATE_QUERIES, payload: [[q], [r]] });
+        })
+        // let r = document.getElementById("response").value;
+        
     }
 
     return(
@@ -35,6 +82,7 @@ function TestsView() {
             EXPECTED RESPONSE    
             </textarea>
             <button onClick={addQuery}>Add Query</button>
+            <p>{console.log("queries: ",queries)}</p>
         </div>
          
     );

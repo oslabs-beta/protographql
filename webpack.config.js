6562d80d34f7b4a3b45f2b33071300aa3edc3c0e
Milmoody <moody.lillian@gmail.com>
2019-09-07 16:44:11 -0700

    added babelrc config to get around bug with async pg query

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-  entry: "./src/index.jsx",
+  entry: ["@babel/polyfill","./src/index.jsx"],
   target: 'electron-main',
   output: {
     publicPath: '/public/',

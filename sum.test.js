0943b1c333a07b1bf0c8e76124c1632cc82f5aad
Vance Wallace <vancito@gmail.com>
2019-09-10 15:23:06 -0700

    removing example jest test

--- sum.test.js
+++
@@ -1,9 +0,0 @@
-const {sum,hiName} = require('./sum');
-
-test('adds 1 and 2 to equal 3', () => {
-    expect(sum(1,2)).toBe(3);
-});
-
-test('Say hello to Debbie', ()=> {
-    expect(hiName("Debbie")).toBe("Hello Debbie");
-});
